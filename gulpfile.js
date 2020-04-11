6e949b74c3d9a2ea6c613e87bd92beda57b60cf0
Hai Phan <hai.phan@gmail.com>
2020-04-11 12:44:02 -0700

    remove gulp-util deprecated

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 
 const zip = require("gulp-zip");
 const gulp = require("gulp");
-const gutil = require("gulp-util");
 const chalk = require("chalk");
 
 gulp.task("default", function() {
-	gutil.log(chalk.yellow("Make sure binaries have execute permission before packaging."));
-	gutil.log(chalk.yellow("This should be run from a Linux shell so that permissions are properly copied into the package."));
+	console.log(chalk.yellow("Make sure binaries have execute permission before packaging."));
+	console.log(chalk.yellow("This should be run from a Linux shell so that permissions are properly copied into the package."));
 
 	return gulp.src(["*.js", "package.json", "package-lock.json", "bin/*"], {base: "."})
 		.pipe(zip("package.zip"))
